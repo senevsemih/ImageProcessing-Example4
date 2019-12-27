5d91365bf1385900058e4c491c746cc230443d16
senevsemih <37352722+senevsemih@users.noreply.github.com>
2019-12-27 15:07:48 +0300

    ImageProcessing

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace görüntüIslemeO4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bnt_gray = new System.Windows.Forms.Button();
+            this.btn_bitmap = new System.Windows.Forms.Button();
+            this.btn_erosion = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.setpix1 = new System.Windows.Forms.Button();
+            this.setpix2 = new System.Windows.Forms.Button();
+            this.tersSim = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bnt_gray
+            // 
+            this.bnt_gray.Location = new System.Drawing.Point(45, 464);
+            this.bnt_gray.Name = "bnt_gray";
+            this.bnt_gray.Size = new System.Drawing.Size(120, 46);
+            this.bnt_gray.TabIndex = 0;
+            this.bnt_gray.Text = "GrayScale";
+            this.bnt_gray.UseVisualStyleBackColor = true;
+            this.bnt_gray.Click += new System.EventHandler(this.bnt_gray_Click);
+            // 
+            // btn_bitmap
+            // 
+            this.btn_bitmap.Location = new System.Drawing.Point(199, 464);
+            this.btn_bitmap.Name = "btn_bitmap";
+            this.btn_bitmap.Size = new System.Drawing.Size(121, 46);
+            this.btn_bitmap.TabIndex = 1;
+            this.btn_bitmap.Text = "Bitmap";
+            this.btn_bitmap.UseVisualStyleBackColor = true;
+            this.btn_bitmap.Click += new System.EventHandler(this.btn_bitmap_Click);
+            // 
+            // btn_erosion
+            // 
+            this.btn_erosion.Location = new System.Drawing.Point(354, 464);
+            this.btn_erosion.Name = "btn_erosion";
+            this.btn_erosion.Size = new System.Drawing.Size(118, 46);
+            this.btn_erosion.TabIndex = 2;
+            this.btn_erosion.Text = "Erosion";
+            this.btn_erosion.UseVisualStyleBackColor = true;
+            this.btn_erosion.Click += new System.EventHandler(this.btn_erosion_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(45, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // setpix1
+            // 
+            this.setpix1.Location = new System.Drawing.Point(773, 45);
+            this.setpix1.Name = "setpix1";
+            this.setpix1.Size = new System.Drawing.Size(126, 42);
+            this.setpix1.TabIndex = 4;
+            this.setpix1.Text = "Set Piksel + ";
+            this.setpix1.UseVisualStyleBackColor = true;
+            this.setpix1.Click += new System.EventHandler(this.setpix1_Click);
+            // 
+            // setpix2
+            // 
+            this.setpix2.Location = new System.Drawing.Point(773, 93);
+            this.setpix2.Name = "setpix2";
+            this.setpix2.Size = new System.Drawing.Size(126, 42);
+            this.setpix2.TabIndex = 5;
+            this.setpix2.Text = "Set Piksel -";
+            this.setpix2.UseVisualStyleBackColor = true;
+            this.setpix2.Click += new System.EventHandler(this.setpix2_Click);
+            // 
+            // tersSim
+            // 
+            this.tersSim.Location = new System.Drawing.Point(773, 141);
+            this.tersSim.Name = "tersSim";
+            this.tersSim.Size = new System.Drawing.Size(126, 38);
+            this.tersSim.TabIndex = 6;
+            this.tersSim.Text = "Ters Simetri";
+            this.tersSim.UseVisualStyleBackColor = true;
+            this.tersSim.Click += new System.EventHandler(this.tersSim_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(596, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(545, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Değer : ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(548, 271);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 42);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Resim Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(967, 544);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tersSim);
+            this.Controls.Add(this.setpix2);
+            this.Controls.Add(this.setpix1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_erosion);
+            this.Controls.Add(this.btn_bitmap);
+            this.Controls.Add(this.bnt_gray);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bnt_gray;
+        private System.Windows.Forms.Button btn_bitmap;
+        private System.Windows.Forms.Button btn_erosion;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button setpix1;
+        private System.Windows.Forms.Button setpix2;
+        private System.Windows.Forms.Button tersSim;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace görüntüIslemeO4
+{
+    public partial class Form1 : Form
+    {
+        Bitmap resim = new Bitmap(300, 300);
+        Bitmap resim_grayScale = new Bitmap(300, 300);
+        Bitmap resim_bitMap = new Bitmap(300, 300);
+        Bitmap resim_erosion = new Bitmap(300, 300);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = new Bitmap(openFileDialog1.FileName);
+            }
+            Graphics.FromImage(resim).DrawImage(pictureBox1.Image, 0, 0, pictureBox1.Width, pictureBox1.Height);
+            Graphics.FromImage(resim_grayScale).DrawImage(pictureBox1.Image, 0, 0, pictureBox1.Width, pictureBox1.Height);
+            Graphics.FromImage(resim_bitMap).DrawImage(pictureBox1.Image, 0, 0, pictureBox1.Width, pictureBox1.Height);
+            Graphics.FromImage(resim_erosion).DrawImage(pictureBox1.Image, 0, 0, pictureBox1.Width, pictureBox1.Height);
+        }
+
+        private void bnt_gray_Click(object sender, EventArgs e)
+        {
+            Color renk;
+            int r, g, b;
+            int gray;
+            for (int x = 0; x < 300; x++)
+            {
+                for (int y = 0; y < 300; y++)
+                {
+                    renk = resim.GetPixel(x, y);
+                    r = Convert.ToInt32(renk.R);
+                    g = Convert.ToInt32(renk.G);
+                    b = Convert.ToInt32(renk.B);
+
+                    gray = (r + g + b) / 3;
+                    resim_grayScale.SetPixel(x, y, Color.FromArgb(gray, gray, gray));
+                }
+                pictureBox1.Image = resim_grayScale;
+                this.Refresh();
+            }
+        }
+
+        private void btn_bitmap_Click(object sender, EventArgs e)
+        {
+            double bit = 0;
+            for (int y = 0; y < resim_bitMap.Height; y++)
+            {
+                for (int x = 0; x < resim_bitMap.Width; x++)
+                {
+                    bit += resim_bitMap.GetPixel(x, y).GetBrightness();
+                }
+            }
+
+            bit = bit / (resim_bitMap.Width * resim_bitMap.Height);
+            bit = bit < .3 ? .3 : bit;
+            bit = bit > .7 ? .7 : bit;
+
+            for (int y = 0; y < resim_bitMap.Height; y++)
+            {
+                for (int x = 0; x < resim_bitMap.Width; x++)
+                {
+                    if (resim_bitMap.GetPixel(x, y).GetBrightness() > bit) resim_bitMap.SetPixel(x, y, Color.White);
+                    else resim_bitMap.SetPixel(x, y, Color.Black);
+                }
+            }
+            pictureBox1.Image = resim_bitMap;
+            this.Refresh();
+        }
+
+        private void setpix1_Click(object sender, EventArgs e)
+        {
+            Color c;
+            int r, g, b;
+            int anti_renk;
+            anti_renk = Convert.ToInt32(textBox1.Text);
+            for (int i = 0; i < 300; i++)
+            {
+                for (int j = 0; j < 300; j++)
+                {
+                    c = resim_grayScale.GetPixel(i, j);
+                    r = c.R + anti_renk;
+                    if (r > 255) r = 255;
+                    g = c.R + anti_renk;
+                    if (g > 255) g = 255;
+                    b = c.R + anti_renk;
+                    if (b > 255) b = 255;
+                    resim_grayScale.SetPixel(i, j, Color.FromArgb(r, g, b));
+
+                }
+            }
+            pictureBox1.Image = resim_grayScale;
+            this.Refresh();
+        }
+
+        private void setpix2_Click(object sender, EventArgs e)
+        {
+            Color c;
+            int r, g, b;
+            int anti_renk;
+            anti_renk = Convert.ToInt32(textBox1.Text);
+            for (int i = 0; i < 300; i++)
+            {
+                for (int j = 0; j < 300; j++)
+                {
+                    c = resim_grayScale.GetPixel(i, j);
+                    r = c.R - anti_renk;
+                    if (r < 0) r = 0;
+                    g = c.R - anti_renk;
+                    if (g < 0) g = 0;
+                    b = c.R - anti_renk;
+                    if (b < 0) b = 0;
+                    resim_grayScale.SetPixel(i, j, Color.FromArgb(r, g, b));
+
+                }
+            }
+            pictureBox1.Image = resim_grayScale;
+            this.Refresh();
+        }
+
+        private void tersSim_Click(object sender, EventArgs e)
+        {
+            Color c;
+            for (int i = 0; i < 300; i++)
+            {
+                for (int j = 0; j < 300; j++)
+                {
+                    c = resim_grayScale.GetPixel(i,j);
+                    Color tmp = resim_grayScale.GetPixel(300 - i - 1, j);
+                    resim_grayScale.SetPixel(i, j, tmp);
+                    resim_grayScale.SetPixel(300 - i - 1, j, c);
+                }
+            }
+            pictureBox1.Image = resim_grayScale;
+            this.Refresh();
+        }
+
+        private void btn_erosion_Click(object sender, EventArgs e)
+        {
+            int x1, x2, x3;
+            x1 = 255;
+            x2 = 255; 
+            x3 = 255;
+            Color cl;
+            int b1, b2, b3;
+            for (int y = 1; y < 299 - 1; y++)
+            {
+                for (int x = 1; x < 299 - 1; x++)
+                {
+                    cl = resim_bitMap.GetPixel(x, y);
+                    b1 = cl.R;
+                    cl = resim_bitMap.GetPixel(x - 1, y);
+                    b2 = cl.R;
+                    cl = resim_bitMap.GetPixel(x + 1, y);
+                    b3 = cl.R;
+                    if (b1 == x1 && b2 == x2 && b3 == x3)
+                    {
+                        resim_erosion.SetPixel(x, y, Color.FromArgb(255, 255, 255));
+                    }
+                    else
+                    {
+                        resim_erosion.SetPixel(x, y, Color.FromArgb(0, 0, 0));
+                    }
+
+                }
+            }
+            pictureBox1.Image = resim_erosion;
+            this.Refresh();
+
+        }
+    }
+}
